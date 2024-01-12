5b307c4ded4c460004c2dd9afe44e88825cc2ca1
Indominuz18 <manmitsingh2003@gmail.com>
2024-01-12 15:24:45 -0800

    Initial Commit

---
+++ IndoCard/IndoCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndoCardApp.swift
+//  IndoCard
+//
+//  Created by Manmeet Singh on 2024-01-12.
+//
+
+import SwiftUI
+
+@main
+struct IndoCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
