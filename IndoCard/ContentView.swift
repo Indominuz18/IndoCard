d8cc3d9c1c45acdf14ec9d087a82e4263bc09a93
Indominuz18 <manmitsingh2003@gmail.com>
2024-06-02 12:17:34 -0700

    Completed Project!

--- IndoCard/ContentView.swift
+++ IndoCard/ContentView.swift
@@ -9,16 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .ignoresSafeArea(.all)
+            VStack {
+                Image("indo").resizable().aspectRatio(contentMode: .fit).frame(width: 200, height: 200).clipShape(Circle()).overlay(Circle().stroke(Color.white, lineWidth: 5))
+                Text("Manmeet Singh").font(Font.custom("Pacifico-Regular", size: 40)).foregroundColor(Color.white).bold()
+                Text("IOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                    
+                InfoView(text: "+1 (604)-379-8123", imagename: "phone.fill")
+                InfoView(text: "manmitsingh2003@gmail.com", imagename: "envelope.fill")
+                
+                
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
--- IndoCard/InfoView.swift
+++ IndoCard/InfoView.swift
@@ -8,11 +8,20 @@
 import SwiftUI
 
 struct InfoView: View {
+    let text: String
+    let imagename: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RoundedRectangle(cornerRadius: 25)
+            .frame(height: 50.0)
+            .foregroundColor(.white)
+            .overlay(HStack{Image(systemName: imagename).foregroundColor(.green)
+                Text(text)}).foregroundColor(.black).padding(.all)
     }
 }
 
+
+
 #Preview {
-    InfoView()
+    InfoView(text: "Hello", imagename: "phone.fill").previewLayout(.sizeThatFits)
 }
