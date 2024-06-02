1861582614fc9f965990498ae362c05226fe5a76
Indominuz18 <manmitsingh2003@gmail.com>
2024-06-02 12:17:09 -0700

    Completed App

---
+++ IndoCard/InfoView.swift
@@ -0,0 +1,18 @@
+//
+//  InfoView.swift
+//  IndoCard
+//
+//  Created by Manmeet Singh on 2024-01-13.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoView()
+}
